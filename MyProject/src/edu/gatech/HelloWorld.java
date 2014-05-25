03b4d2634978134f5f0c346bc3aba31f80b15699
sharris013 <stephennharris@bellsouth.net>
2014-05-25 13:30:29 -0400

    Updated to do the add of two parameters

--- MyProject/src/edu/gatech/HelloWorld.java
+++ MyProject/src/edu/gatech/HelloWorld.java
@@ -3,7 +3,15 @@ package edu.gatech;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		// Get two command line parameters
+		Integer a = Integer.parseInt(args[0]);
+		Integer b = Integer.parseInt(args[1]);
+		
+		// Add the two parameters and store in sum
+		Integer sum = a+b;
+		
+		// Display the sum of the two parameters
+		System.out.println(a + " + " + b + " = " + sum);		
 	}
 
 }
